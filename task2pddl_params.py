22648ec06261093184eace5272a1ba9af6a02dae
uyeongkim <uyeongkim@gm.gist.ac.kr>
2022-09-19 11:59:37 +0000

    can deal with gpt2 now

--- task2pddl_params.py
+++ task2pddl_params.py
@@ -1,19 +1,33 @@
+"""
+This program predicts PDDL parameters for a given task description.
+This is done by finding the most similar task description in the ALFRED train dataset.
+"""
+
+import json
+import os
+import argparse
+from selectors import EpollSelector
 import numpy as np
 import torch
 from sentence_transformers import SentenceTransformer
 from sentence_transformers import util as st_utils
-import json
-import os
-import argparse
+from transformers import GPT2Tokenizer, GPT2Model
 
-with open('data/task2param.json', 'r') as f:
+with open('data/task2param.json', 'r', encoding='utf-8') as f:
     TASK2PARAM = json.load(f)
-with open('data/available_examples.json', 'r') as f:
+with open('data/available_examples.json', 'r', encoding='utf-8') as f:
     EXAMPLES = json.load(f)
+EXAMPLE_TASK_LIST = [example.split('\n')[0].split(':')[1].strip() for example in EXAMPLES]
+with open('data/supported_sentence_transformer.txt', 'r', encoding='utf-8') as f:
+    _SUPPORTED = f.read().splitlines()
+SUPPORTED_MODEL = [model.split('\n')[0] for model in _SUPPORTED]
 
-# helper function for finding similar sentence in a corpus given a query
-def find_most_similar(query_str, corpus_embedding):
-    query_embedding = translation_lm.encode(query_str, convert_to_tensor=True, device=device)
+def find_most_similar(query_str, corpus_embedding, translation_lm, supported = True):
+    """helper function for finding similar sentence in a corpus given a query"""
+    if not supported:
+        query_embedding = translation_lm(query_str)
+    else:
+        query_embedding = translation_lm.encode(query_str, convert_to_tensor=True, device=device)
     # calculate cosine similarity against each candidate sentence in the corpus
     cos_scores = st_utils.pytorch_cos_sim(query_embedding, corpus_embedding)[0].detach().cpu().numpy()
     # retrieve high-ranked index and similarity score
@@ -21,25 +35,24 @@ def find_most_similar(query_str, corpus_embedding):
     return most_similar_idx, matching_score
 
 def calculate_pddl_similarity(p1, p2):
+    """Returns if two PDDL parameters concide"""
     pddl_scores = {}
     all = True
-    for p, t in p1.items():
+    for p in p1:
         pddl_scores[p] = p1[p] == p2[p]
         if all and not pddl_scores[p]:
             all = False
     pddl_scores['all'] = all
     return pddl_scores
 
-def get_example_taskList():
-    example_task_list = [example.split('\n')[0] for example in EXAMPLES]  # first line contains the task name
-    return example_task_list
-
-def find_best_pddl(task, example_task_embedding):
-    example_idx, matching_score = find_most_similar(task, example_task_embedding)
-    example = available_examples[example_idx].split('\n')[0].split(':')[1].strip()
+def find_best_pddl(task, example_task_embedding, translation_lm):
+    if len(task) == 0:
+        raise ValueError('Task description is empty')
+    example_idx, _ = find_most_similar(task, example_task_embedding, translation_lm)
+    example = EXAMPLE_TASK_LIST[example_idx]
     return TASK2PARAM[example]
 
-def save_pddl_match(example_task_embedding):
+def save_pddl_match(example_task_embedding, translation_lm, filename='data/pddl_match.json'):
     pddl_match = {}
     for split in ['valid_seen', 'valid_unseen']:
         data_path = '../alfred/data/json_2.1.0/{sp}'.format(sp=split)
@@ -47,10 +60,23 @@ def save_pddl_match(example_task_embedding):
             for trial in os.listdir(os.path.join(data_path, ep)):
                 with open(os.path.join(data_path, ep, trial, 'traj_data.json'), 'r') as f:
                     data = json.load(f)
-                anns = data['turk_annotations']['anns']
-                pddl_match.update({ann['task_desc']: find_best_pddl(ann['task_desc'], example_task_embedding) for ann in anns})
-    with open('pddl_match.json', 'w') as f:
+                anns = [ann['task_desc'] for ann in data['turk_annotations']['anns']]
+                _anns = []
+                for ann in anns:
+                    if '\n' in ann:
+                        _anns.extend(a.strip().replace('\b', '') for a in ann.split('\n') if a != '')
+                    else:
+                        _anns.append(ann.strip().replace('\b', ''))
+                anns = _anns
+                pddl_match.update({ann: find_best_pddl(ann, example_task_embedding, translation_lm) for ann in anns})
+    with open(filename, 'w') as f:
         json.dump(pddl_match, f, indent=4)
+
+def gpt_encoder(sentence):
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2').to(device)
+    model  = GPT2Model.from_pretrained('gpt2').to(device)
+    token = tokenizer(sentence, return_tensors='pt').to(device)
+    return model(**token)[0].mean(dim=1)
 
 if __name__ == '__main__':
     GPU = 0
@@ -67,15 +93,24 @@ if __name__ == '__main__':
     # initialize Translation LM
     try:
         translation_lm = SentenceTransformer(args.translation_lm).to(device)
-    except Exception as e:
-        print('model not in huggingface')
-        print(e)
-        exit()
-
-    # create example task embeddings using Translated LM
-    example_task_list = get_example_taskList()
-    example_task_embedding = translation_lm.encode(example_task_list, batch_size=512, convert_to_tensor=True, device=device)  # lower batch_size if limited by GPU memory
+        word_embedding_model = translation_lm._first_module()
+        word_embedding_model.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+        word_embedding_model.auto_model.resize_token_embeddings(len(word_embedding_model.tokenizer))
+        # create example task embeddings using Translated LM
+        example_task_embedding = translation_lm.encode(EXAMPLE_TASK_LIST, batch_size=512, convert_to_tensor=True, device=device)  # lower batch_size if limited by GPU memory
+        if args.translation_lm not in SUPPORTED_MODEL:
+            with open('data/supported_sentence_transformer.txt', 'a') as f:
+                f.write(args.translation_lm + '\n')
+            SUPPORTED_MODEL.append(args.translation_lm)
+    except NameError:
+        print('model:{model} not in huggingface'.format(model=args.translation_lm))
+        if args.translation_lm == 'gpt2':
+            outputs = [gpt_encoder(task) for task in EXAMPLE_TASK_LIST]
+            example_task_embedding = torch.cat(outputs, dim=0)
+            translation_lm = gpt_encoder
+        else:
+            raise ValueError('model{model} not in huggingface'.format(model=args.translation_lm))
 
     # save matching pddl_params
-    save_pddl_match(example_task_embedding)
+    save_pddl_match(example_task_embedding, filename='data/gpt-pddl_match.json', translation_lm=translation_lm)
     
